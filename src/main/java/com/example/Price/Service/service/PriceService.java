9e07ff5a16c9b79dc944582adf0ef0a4b8aa658d
maxim.irmscher <maxim.irmscher@idealo.de>
2022-05-20 10:58:16 +0200

    KK-33 implements logic + test

--- src/main/java/com/example/Price/Service/service/PriceService.java
+++ src/main/java/com/example/Price/Service/service/PriceService.java
@@ -1,4 +1,19 @@
 package com.example.Price.Service.service;
 
+import com.example.Price.Service.dto.PriceRequest;
+import com.example.Price.Service.entity.ProductComponent;
+
 public class PriceService {
+
+
+    public PriceRequest sumComponentPrices(PriceRequest priceRequest) {
+
+         return priceRequest.setTotalPrice(
+                 priceRequest
+                .getComponents()
+                .stream()
+                .map(ProductComponent::getPrice)
+                .reduce(0L, Long::sum)
+         );
+    }
 }
