ad5e372c998d5a7d99efce450479502fa324f85d
maxim.irmscher <maxim.irmscher@idealo.de>
2022-05-20 10:41:12 +0200

    KK-20 adds dto/PriceRequest

---
+++ src/main/java/com/example/Price/Service/dto/PriceRequest.java
@@ -0,0 +1,27 @@
+package com.example.Price.Service.dto;
+
+import com.example.Price.Service.entity.Product;
+import com.example.Price.Service.entity.ProductComponent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class PriceRequest {
+
+    private long id;
+    private String name;
+    private Product product;
+    private List<ProductComponent> components;
+    private long totalPrice;
+
+
+}
