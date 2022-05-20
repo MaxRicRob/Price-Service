b94de84e1d1a9e9c5d0083bdf040e792c2247bcb
maxim.irmscher <maxim.irmscher@idealo.de>
2022-05-20 10:37:37 +0200

    KK-20 adds Product

---
+++ src/main/java/com/example/Price/Service/entity/Product.java
@@ -0,0 +1,20 @@
+package com.example.Price.Service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Product {
+
+    private long id;
+    private String name;
+    private String components;
+}
