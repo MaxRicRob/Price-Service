a34efee5c3bc33117f792de855ef4f8a2f60f7fd
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-06 09:46:40 +0200

    small fix

--- src/test/java/com/example/PriceService/api/RabbitControllerTest.java
+++ src/test/java/com/example/PriceService/api/RabbitControllerTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.when;
 class RabbitControllerTest {
 
     @InjectMocks
-    RabbitController rabbitController = new RabbitController();
+    RabbitController rabbitController;
 
     @Mock
     PriceService priceService;
